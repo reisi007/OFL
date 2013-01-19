26b373a7d38e8abaec1ebee55d09e4307b657c54
Florian Reisinger <reisi007@gmail.com>
2013-01-19 16:48:57 +0100

    First part of big Update

--- FB OrderedFriendsList/Form2.Designer.cs
+++ FB OrderedFriendsList/Form2.Designer.cs
@@ -29,56 +29,82 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.progress = new System.Windows.Forms.ProgressBar();
+            this.user = new System.Windows.Forms.TextBox();
+            this.pass = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Location = new System.Drawing.Point(13, 227);
+            this.button1.Location = new System.Drawing.Point(13, 124);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(270, 23);
+            this.button1.Size = new System.Drawing.Size(223, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "Okay";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // textBox1
+            // progress
             // 
-            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
+            this.progress.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox1.Location = new System.Drawing.Point(13, 30);
-            this.textBox1.MaxLength = 450000;
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(270, 191);
-            this.textBox1.TabIndex = 1;
+            this.progress.Location = new System.Drawing.Point(13, 95);
+            this.progress.Name = "progress";
+            this.progress.Size = new System.Drawing.Size(223, 23);
+            this.progress.TabIndex = 3;
             // 
-            // linkLabel1
+            // user
             // 
-            this.linkLabel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.user.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Location = new System.Drawing.Point(13, 11);
-            this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(269, 13);
-            this.linkLabel1.TabIndex = 2;
-            this.linkLabel1.TabStop = true;
-            this.linkLabel1.Text = "Copy source of this page into the textfild and click Okay";
-            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            this.user.Location = new System.Drawing.Point(12, 25);
+            this.user.Name = "user";
+            this.user.Size = new System.Drawing.Size(224, 20);
+            this.user.TabIndex = 4;
+            // 
+            // pass
+            // 
+            this.pass.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pass.Location = new System.Drawing.Point(12, 64);
+            this.pass.Name = "pass";
+            this.pass.PasswordChar = '*';
+            this.pass.Size = new System.Drawing.Size(224, 20);
+            this.pass.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "FB-Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Password:";
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(295, 262);
-            this.Controls.Add(this.linkLabel1);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(248, 159);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pass);
+            this.Controls.Add(this.user);
+            this.Controls.Add(this.progress);
             this.Controls.Add(this.button1);
-            this.MinimumSize = new System.Drawing.Size(311, 300);
             this.Name = "Form2";
             this.Text = "FB Ordered Friends List";
             this.Load += new System.EventHandler(this.Form2_Load);
@@ -90,7 +116,10 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.TextBox user;
+        private System.Windows.Forms.TextBox pass;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        public System.Windows.Forms.ProgressBar progress;
     }
 }
--- FB OrderedFriendsList/Form2.cs
+++ FB OrderedFriendsList/Form2.cs
@@ -9,62 +9,184 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Net;
 using System.Diagnostics;
+using System.IO;
 
 namespace FB_OrderedFriendsList
 {
     public partial class Form2 : Form
     {
+        public manage_people storage = new manage_people();
+        private BackgroundWorker init_bg_worker()
+        {
+            BackgroundWorker bg_w = new BackgroundWorker();
+            bg_w.DoWork += get_urls;
+            bg_w.RunWorkerCompleted += worker_ready;
+            return bg_w;
+        }
+        private CookieCollection cookies { get; set; }
         public Form2()
         {
-            
+
             InitializeComponent();
-            
+            cookies = new CookieCollection();
+
         }
-        
+
         private void Form2_Load(object sender, EventArgs e)
         {
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://www.facebook.com/");
+                request.CookieContainer = new CookieContainer();
+                request.CookieContainer.Add(cookies);
+                //Get the response from the server and save the cookies from the first request..
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                cookies = response.Cookies;
+            }
+            catch (Exception ex)
+            { MessageBox.Show(ex.Message, "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error); Application.Exit(); }
+        }
+        private string getfbpage(string url)
+        {
+            progress.Maximum = 4;
+            progress.Value = 1;
+            string getUrl = url;
+            string postData = String.Format("email={0}&pass={1}", user.Text, pass.Text);
+            HttpWebRequest getRequest = (HttpWebRequest)WebRequest.Create(getUrl);
+            progress.Value = 2;
+            getRequest.CookieContainer = new CookieContainer();
+            getRequest.CookieContainer.Add(cookies); //recover cookies First request
+            getRequest.Method = WebRequestMethods.Http.Post;
+            getRequest.UserAgent = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.874.121 Safari/535.2";
+            getRequest.AllowWriteStreamBuffering = true;
+            getRequest.ProtocolVersion = HttpVersion.Version11;
+            getRequest.AllowAutoRedirect = true;
+            getRequest.ContentType = "application/x-www-form-urlencoded";
+            byte[] byteArray = Encoding.ASCII.GetBytes(postData);
+            getRequest.ContentLength = byteArray.Length;
+            Stream newStream = getRequest.GetRequestStream(); //open connection
+            newStream.Write(byteArray, 0, byteArray.Length); // Send the data.
+            newStream.Close();
+            HttpWebResponse getResponse = (HttpWebResponse)getRequest.GetResponse();
+            progress.Value = 3;
+            using (StreamReader sr = new StreamReader(getResponse.GetResponseStream()))
+            {
+                return sr.ReadToEnd();
+            }
+
+        }
+        private void getfbpage(string url, out people person)
+        {
+            person = new people();
+            try
+            {
+                WebClient web = new WebClient();
+                person.url = url;
+                url = "https://graph.facebook.com/" + url;
+                string httpfile = web.DownloadString(url);
+                int i_name = httpfile.IndexOf("name") + 7;
+                string s_name = httpfile.Remove(0, i_name);
+                i_name = s_name.IndexOf(",") - 1;
+                person.name = s_name.Remove(i_name);
+                int g_index = httpfile.IndexOf("gender") + 5;
+                httpfile = httpfile.Remove(0, g_index);
+                httpfile = httpfile.Remove(10);
+                if (!httpfile.Contains("f"))
+                    person.male = true;
+                else
+                    person.male = false;
+            }
+            catch (Exception)
+            { }
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        public void button1_Click(object sender, EventArgs e)
         {
-          string  html_txt = textBox1.Text;
-          int i = html_txt.IndexOf("OrderedFriends") +44 ;
+            string html_txt = getfbpage("https://www.facebook.com/login.php?next=https%3A%2F%2Fwww.facebook.com%2Fhome.php");
+            progress.Value = 4;
+            int i = html_txt.IndexOf("InitialChatFriendsList") + 37;
             html_txt = html_txt.Remove(0, i);
-            i = html_txt.IndexOf("]")-1;
-            html_txt =  html_txt.Remove(i);
-            html_txt = html_txt.Replace("\"" +"," +"\"", ",");
-            string[] seperator = new string[] { ","};
+            i = html_txt.IndexOf("]") - 1;
+            html_txt = html_txt.Remove(i);
+            progress.Value = 0;
+            html_txt = html_txt.Replace("\"" + "," + "\"", ",");
+            string[] seperator = new string[] { "," };
             string[] sfb_id = html_txt.Split(seperator, System.StringSplitOptions.RemoveEmptyEntries);
-// Testing 
-          /*  string output = "";
-            for (int j = 0; j < sfb_id.Length; j++)
+            // Testing 
+            /*  string output = "";
+              for (int j = 0; j < sfb_id.Length; j++)
+              {
+                 // fb_id[j] = Convert.ToInt32(sfb_id[j]);
+                  output += sfb_id[j] + Environment.NewLine;
+              }
+
+              MessageBox.Show(output);*/
+            progress.Maximum = sfb_id.Length;
+            for (int k = 0; k < sfb_id.Length; k++)
             {
-               // fb_id[j] = Convert.ToInt32(sfb_id[j]);
-                output += sfb_id[j] + Environment.NewLine;
+                multithread value = new multithread();
+                value.array_pos = k;
+                value.url = sfb_id[k];
+                BackgroundWorker get_url = init_bg_worker();
+                get_url.RunWorkerAsync(value);
             }
-
-            MessageBox.Show(output);*/
-
-            string html = "<html> <head>  <meta content=\"text/html; charset=ISO-8859-1\" http-equiv=\"content-type\"><title>Your Friends</title></head><body>";
-
-            i = 1;
-            foreach (string s in sfb_id)
-            {
-                html += "<a href=https://www.facebook.com/" + s +">"+ i + ". Friend" +"</a><br>" + Environment.NewLine;
-                i++;
-            }
-            html += "</body></html>";
-            string path = System.IO.Path.GetTempPath() + "\\file.html";
-            System.IO.File.WriteAllText(path, html);
-            Process.Start(path);
-
-            
         }
-
-        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        private void get_urls(object sender, DoWorkEventArgs e)
         {
-            Process.Start("https://www.facebook.com/");
+            multithread values = (multithread)e.Argument;
+            people person;
+            getfbpage(values.url, out person);
+            person.index = values.array_pos++;
+            storage.warehouse.Add(person);
+            progress.BeginInvoke(new MethodInvoker(increase_progress));
+
         }
+
+        private void increase_progress()
+        {
+            progress.Value++;
+        }
+
+        private void worker_ready(object sender, RunWorkerCompletedEventArgs e)
+        {
+            
+           if (progress.Value == progress.Maximum)
+            {
+               
+                string html = "<html> <head>  <meta content=\"text/html; charset=ISO-8859-1\" http-equiv=\"content-type\"><title>Your Friends</title></head><body>";
+                foreach (people s in storage.warehouse)
+                {
+
+                    html += "<a href=https://www.facebook.com/" + s.url + ">" + s.name+ "("+s.index + ". Friend)" + "</a><br>" + Environment.NewLine;
+                   
+                }
+                html += "</body></html>";
+                string path = System.IO.Path.GetTempPath() + "\\file.html";
+                System.IO.File.WriteAllText(path, html);
+                Process.Start(path);
+            }
+        }
+    }
+    public struct multithread
+    {
+        public int array_pos;
+        public string url;
+        public people person;
+    }
+    public class people
+    {
+        public int index;
+        public string name;
+        public bool male;
+        public string url;
+    }
+    public class manage_people
+    {
+        public manage_people()
+        {
+            warehouse = new List<people>();
+        }
+       public List<people> warehouse;
     }
 }
